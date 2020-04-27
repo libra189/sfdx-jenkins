81c02c377755bb32d92fac44aaea26ce0e141ef6
libra189 <libra189@gmail.com>
2020-04-27 09:25:47 +0900

    チェックを付けた値をJSで取得するサンプルを追加

--- force-app/main/default/lwc/sampleLWC/sampleLWC.js
+++ force-app/main/default/lwc/sampleLWC/sampleLWC.js
@@ -1,7 +1,29 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
+import addContacts from '@salesforce/apex/AccountDetail.addContacts'; // 取引先責任者を作成するApexメソッド
+
 export default class HelloWorld extends LightningElement {
-    @track greeting = 'World';
-    changeHandler(event) {
-        this.greeting = event.target.value;
+    save() {
+        // チェックボックス要素をリストで取得
+        const elements = this.template.querySelectorAll('lightning-input');
+        console.debug(elements);
+
+        const contacts = []; // チェックのついている項目のみを格納する配列
+        elements.forEach(elem => {
+            if (elem.checked) {
+                // チェックの付いているデータのみを配列に格納
+                const val = elem.dataset.value;
+                contacts.push(val);
+                console.debug(val);
+            }
+        });
+
+        console.debug(contacts);
+        addContacts({contactNames: contacts})
+        .then(result => {
+            console.debug(result);
+        })
+        .catch(error => {
+            console.error(error);
+        })
     }
 }
